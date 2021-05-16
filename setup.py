799adc67c8f44de80554c82a7ba38c4b88e0fb18
Eric Park <me@ericswpark.com>
2021-05-16 14:00:24 +0900

    Initial packaging efforts

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+from shippy.constants import VERSION_STRING
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shippy",
+    version=VERSION_STRING,
+    author="Eric Park",
+    author_email="me@ericswpark.com",
+    description="Client-side tool to interface with shipper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ericswpark/shippy",
+    project_urls={
+        "Bug Tracker": "https://github.com/ericswpark/shippy/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages='shippy',
+    python_requires=">=3.6",
+)
