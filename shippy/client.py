4c5c8abfa69605534df0352f906f81ae024d09a6
Eric Park <me@ericswpark.com>
2021-05-19 03:21:02 +0900

    client: specify detail parameter in UploadException
    Otherwise it doesn't work...

--- shippy/client.py
+++ shippy/client.py
@@ -90,7 +90,7 @@ def chunked_upload(server_url, build_file, checksum_file, token):
                     print("Status code received from server: {}".format(r.status_code))
                     with open('output.html', 'wb') as error_output_raw:
                         error_output_raw.write(r.content)
-                raise UploadException("Something went wrong during the upload.", retry=False)
+                raise UploadException(detail="Something went wrong during the upload.", retry=False)
 
     # Complete upload
     r = requests.post(device_upload_url, headers={"Authorization": "Token {}".format(token)},
@@ -103,7 +103,7 @@ def chunked_upload(server_url, build_file, checksum_file, token):
             print("Status code received from server: {}".format(r.status_code))
             with open('output.html', 'wb') as error_output_raw:
                 error_output_raw.write(r.content)
-        raise UploadException("Something went wrong during upload finalization.", retry=False)
+        raise UploadException(detail="Something went wrong during upload finalization.", retry=False)
 
 
 def get_md5_from_file(checksum_file):
@@ -146,22 +146,23 @@ def direct_upload(server_url, build_file, checksum_file, token):
         print("Successfully uploaded the build {}!".format(build_file))
     elif r.status_code == 400:
         if r.json()['error'] == "duplicate_build":
-            raise UploadException("This build already exists in the system!", retry=False)
+            raise UploadException(detail="This build already exists in the system!", retry=False)
         if r.json()['error'] == "missing_files":
-            raise UploadException("One of the required fields are missing!", retry=False)
+            raise UploadException(detail="One of the required fields are missing!", retry=False)
         if r.json()['error'] == "file_name_mismatch":
-            raise UploadException("The build file name does not match the checksum file name!", retry=False)
+            raise UploadException(detail="The build file name does not match the checksum file name!", retry=False)
         if r.json()['error'] == "invalid_file_name":
-            raise UploadException("The file name was malformed!", retry=False)
+            raise UploadException(detail="The file name was malformed!", retry=False)
         if r.json()['error'] == "not_official":
-            raise UploadException("The build is not official!", retry=False)
+            raise UploadException(detail="The build is not official!", retry=False)
         if r.json()['error'] == "codename_mismatch":
-            raise UploadException("The codename does not match the build file name!", retry=False)
+            raise UploadException(detail="The codename does not match the build file name!", retry=False)
     elif r.status_code == 401:
         if r.json()['error'] == "insufficient_permissions":
             raise UploadException(detail="You are not allowed to upload for this device!", retry=False)
     elif r.status_code == 500:
-        raise UploadException("An internal server error occurred. Contact the administrators for help.", retry=True)
+        raise UploadException(detail="An internal server error occurred. Contact the administrators for help.",
+                              retry=True)
     else:
         print("A problem occurred while uploading your build.")
         undef_response_exp(r)
