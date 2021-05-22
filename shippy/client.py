3df6a60e0a94e37bcfa34cbc040bdb7728dc73a0
Eric Park <me@ericswpark.com>
2021-05-23 01:18:45 +0900

    Release 1.3.7
    Changes since 1.3.6:
     - shippy no longer tries multiple times if an upload fails. Previously, this loop caused many bugs. Also, it doesn't make sense to try multiple times if the upload fails for any reason, since we'll get the same failure anyway.
     - shippy now warns if multiple builds have been detected.
     - Made error message more descriptive to help catch more problems.
     - Code cleanup

--- shippy/client.py
+++ shippy/client.py
@@ -161,5 +161,5 @@ def upload_exception_check(r, build_file):
             raise UploadException("You are not allowed to upload for this device!")
     elif r.status_code == 500:
         raise UploadException("Something went wrong with the server. Please contact the admins.")
-    
+
     undef_response_exp(r)
