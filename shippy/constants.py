bfb28d80212cfd0b2a0909fbb5194f00e900c5cf
Eric Park <me@ericswpark.com>
2022-05-07 18:29:35 +0900

    constants: run flake8 and black

--- shippy/constants.py
+++ shippy/constants.py
@@ -1,11 +1,13 @@
-SENTRY_SDK_URL = "https://0da75bab4671455ea1b7580cb93649f5@o444286.ingest.sentry.io/5645833"
+SENTRY_SDK_URL = (
+    "https://0da75bab4671455ea1b7580cb93649f5@o444286.ingest.sentry.io/" "5645833"
+)
 
 SERVER_COMPAT_ERROR_MSG = """
 The server you're connecting to is out-of-date.
 If you know the server admin, please ask them to upgrade the server.
  * Reported server version: \t{}
  * Compatible version: \t\t{}
- 
+
 To prevent data corruption, shippy will not work with an outdated server. Exiting...
 """
 
@@ -28,12 +30,17 @@ We recommend updating shippy with the following command:
 \tpip3 install --upgrade shipper-shippy
 """
 
-RATE_LIMIT_WAIT_STATUS_MSG = "Waiting to resume after being rate limited. shippy will resume uploading in {} seconds."
+RATE_LIMIT_WAIT_STATUS_MSG = (
+    "Waiting to resume after being rate limited. shippy will "
+    "resume uploading in {} seconds."
+)
 
 
 CANNOT_CONTACT_SERVER_ERROR_MSG = "Cannot contact the server. "
 UNEXPECTED_SERVER_RESPONSE_ERROR_MSG = "The server returned an unexpected response. "
-FAILED_TO_RETRIEVE_SERVER_VERSION_ERROR_MSG = "Failed to retrieve server version information! "
+FAILED_TO_RETRIEVE_SERVER_VERSION_ERROR_MSG = (
+    "Failed to retrieve server version information! "
+)
 FAILED_TO_LOG_IN_ERROR_MSG = "Failed to log into server! "
 
 UNHANDLED_EXCEPTION_MSG = """
