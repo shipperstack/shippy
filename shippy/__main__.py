67bb1837837325a131ce6ee2179ffb077e3ed64d
Eric Park <me@ericswpark.com>
2022-11-23 22:02:11 +0900

    main: ignore ConnectionErrors
    Fixes #91

--- shippy/__main__.py
+++ shippy/__main__.py
@@ -31,7 +31,7 @@ from .exceptions import LoginException, UploadException
 from .helper import input_yn, print_error, print_warning, print_success
 from .version import __version__, server_compat_version
 
-ignore_errors = [KeyboardInterrupt]
+ignore_errors = [KeyboardInterrupt, ConnectionError]
 
 sentry_sdk.init(
     SENTRY_SDK_URL,
